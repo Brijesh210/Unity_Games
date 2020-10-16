2c57c661dda0ab82d46ca2f4ed0c5a44d89389ba
Brijesh varsani <varsani340@gmail.com>
2020-10-16 09:52:41 +0200

    Initial/ Final

---
+++ Assets/Script1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class Script1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Light sun;
+    public Light spot_light;
+
+
+    void Start()
+    {
+        spot_light.enabled = false;
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("W was pressed");
+            sun.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("U was pressed");
+            sun.enabled = true;
+        }
+
+
+    }
+
+}
---
+++ Assets/fpsScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml.Schema;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fpsScript : MonoBehaviour
+{
+    private int count;
+    public Text point;
+    public Text win;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        point.text = " Total Point :" + count.ToString();
+        win.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        point.text = " Total Point :" + count.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("box"))
+        {
+
+            count++;
+            other.gameObject.SetActive(false);
+            Debug.Log("collision with box" + "total count: " + count.ToString());
+            point.text = " Total Point :" + count.ToString();
+
+        }
+
+        if (other.gameObject.CompareTag("coin"))
+        {
+            count+=2;
+            other.gameObject.SetActive(false);
+            Debug.Log("collision with coin" + "total count: " + count.ToString());
+            point.text = " Total Point :" + count.ToString();
+        }
+        if(count >5)
+        {
+            Debug.Log("Win!");
+            win.text = "You Won!";
+        }
+        
+
+    }
+}
